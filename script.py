87ce1fbd294f3cf7384010fbffcf2489222ad801
zohaib ashraf <zohaib@nclouds.com>
2022-03-17 02:04:03 +0500

    update in code

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from asyncio import events
+
 import boto3
 
 def main():
@@ -14,19 +14,23 @@ def main():
                 if tag['Key'].lower() == 'updown' \
                     and tag['Value'].lower() == 'march-16-2022':
                     instance_state = instance['State']['Name']
-                    instance_tostop_ids.append(instance['InstanceId'])
-                    instance_tostart_ids.append(instance['InstanceId'])
                     
-    if instance_state == 'running':
-        print("STOP your instances: " + str(instance_tostart_ids)) 
-        ec2.stop_instances(InstanceIds=instance_tostart_ids)
-    elif instance_state == 'stopped':
-         ec2.start_instances(InstanceIds=instance_tostop_ids)
-         print("STart your instances: " + str(instance_tostart_ids)) 
-                        
-                       
-                    
-    
- 
+                    if instance_state == 'running':
+                        instance_tostop_ids.append(instance['InstanceId'])
+                        print("STOP your instances: " + str(instance_tostop_ids)) 
+                        # ec2.stop_instances(InstanceIds=instance_tostart_ids)
+                    elif instance_state == 'stopped':
+                        instance_tostart_ids.append(instance['InstanceId'])
+                        print("STart your instances: " + str(instance_tostart_ids)) 
+                        # ec2.stop_instances(InstanceIds=instance_tostart_ids)
+                        print("STOP your instances: " + str(instance_tostop_ids)) 
+    if len(instance_tostop_ids) > 0:
+        ec2.stop_instances(InstanceIds=instance_tostop_ids)
+        print("instance stop")
+    else:
+      ec2.start_instances(InstanceIds=instance_tostart_ids)
+      print("instance start")
+                                 
 if __name__ == "__main__":
- main()  
+  main()  
+
